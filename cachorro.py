0add979e148bf25f9024c72df97539972508531c
mateusmdc <38740703+mateusmdc@users.noreply.github.com>
2018-12-05 11:11:31 -0300

    classe Cachorro

--- cachorro.py
+++ cachorro.py
@@ -1,22 +1,12 @@
-from cachorro import Cachorro
-from gato import Gato
-from time import sleep
+class Cachorro():
+	def __init__(self, nome, idade, patas):
+		self.nome = nome
+		self.idade = idade
+		self.patas = patas
 
-nome = str(input('Digite o nome: '))
-idade = int(input('digite a idade: '))
-patas = int(input('Digite o numero de patas: '))
-animal1 = Cachorro(nome, idade, patas)
+	def latir(self):
+		print('au au')
 
-
-
-print('Nome do animal: {}'.format(animal1.nome))
-
-for c in range(1,10):
-	print('...',end='', flush=True)
-	sleep(1)
-
-animal1.latir()
-
-variavel = animal1.TrazQntsSapatos()
-
-print(variavel)
+	def TrazQntsSapatos(self):
+		quantidadesapatos = 5
+		return 5
