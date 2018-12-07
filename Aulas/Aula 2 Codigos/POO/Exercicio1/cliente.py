68d5dc49becb86b2d7ba95631cae2eced7e6f800
mateusmdc <38740703+mateusmdc@users.noreply.github.com>
2018-12-07 03:28:20 -0200

    Create cliente.py
    classe Cliente

---
+++ Aulas/Aula 2 Codigos/POO/Exercicio1/cliente.py
@@ -0,0 +1,8 @@
+class Cliente():
+	def __init__(self, nome, peso, altura, idade):
+		self.nome = nome
+		self.peso = peso
+		self.altura = altura
+		self.idade = idade
+	def ciente(self):
+		print('É necessário cuidar da saúde')
