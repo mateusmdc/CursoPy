4eaffe2cc0afaa24f47ceb9cb00e31b847fcb237
mateusmdc <38740703+mateusmdc@users.noreply.github.com>
2018-12-07 03:26:12 -0200

    Create cachorro.py
    Classe cachorro

---
+++ Aulas/Aula 2 Codigos/POO/Exemplos/cachorro.py
@@ -0,0 +1,12 @@
+class Cachorro():
+	def __init__(self, nome, idade, patas):
+		self.nome = nome
+		self.idade = idade
+		self.patas = patas
+
+	def latir(self):
+		print('au au')
+
+	def TrazQntsSapatos(self):
+		quantidadesapatos = 5
+return 5
