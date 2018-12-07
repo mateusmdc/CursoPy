2685a8a67c6316e9acdfdc095ca61efb3bc27090
mateusmdc <38740703+mateusmdc@users.noreply.github.com>
2018-12-07 01:12:59 -0200

    Update cachorro.py

--- Curso Python Aulas/Aula 2 Codigos/POO/cachorro.py
+++ Curso Python Aulas/Aula 2 Codigos/POO/cachorro.py
@@ -1,1 +1,12 @@
-33
+class Cachorro():
+	def __init__(self, nome, idade, patas):
+		self.nome = nome
+		self.idade = idade
+		self.patas = patas
+
+	def latir(self):
+		print('au au')
+
+	def TrazQntsSapatos(self):
+		quantidadesapatos = 5
+		return 5
