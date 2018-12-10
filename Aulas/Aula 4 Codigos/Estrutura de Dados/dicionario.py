615087a4dd2c6775173bbb35c1411627415d3582
mateusmdc <38740703+mateusmdc@users.noreply.github.com>
2018-12-10 11:01:08 -0300

    Update dicionario.py

--- Aulas/Aula 4 Codigos/Estrutura de Dados/dicionario.py
+++ Aulas/Aula 4 Codigos/Estrutura de Dados/dicionario.py
@@ -1,7 +1,35 @@
-jogador = {'nome':'kaka', 'posicao':'meio campo'}
+'''jogador1 = {'nome':'kaka', 'posicao':'meio campo'}
 
-print(jogador.items())
+jogador2 = {'nome':'ronaldo', 'posicao':'atacante'}
+
+jogador3 = {'nome':'neuer', 'posicao':'goleiro'}
+
+jogador4= {'nome':'marcelo', 'posicao': 'lateral'}'''
+
+'''print(jogador.items())
 
 print(jogador.keys())
 
-print(jogador.values())
+print(jogador.values())'''
+
+
+'''time = []
+time.append(jogador1.copy())
+time.append(jogador2.copy())
+time.append(jogador3.copy())
+time.append(jogador4.copy())
+
+print(time)
+print(time[1])'''
+
+time = []
+jogador = {}
+
+for c in range(0,4):
+	jogador['nome'] = str(input('digite o nome do jogador {}: '.format(c+1)))
+	jogador['posicao'] = str(input('digite o posicao do jogador {}: '.format(c+1)))
+	jogador['qtd gols'] = str(input('digite a qtd de gols do jogador {}: '.format(c+1)))
+
+	time.append(jogador.copy())
+
+print(time)
