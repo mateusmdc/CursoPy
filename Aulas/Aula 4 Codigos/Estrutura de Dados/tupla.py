96782e45143dd77f5eaee2f6dc8093c638940bcc
mateusmdc <38740703+mateusmdc@users.noreply.github.com>
2018-12-10 10:44:45 -0300

    Create tupla.py

---
+++ Aulas/Aula 4 Codigos/Estrutura de Dados/tupla.py
@@ -0,0 +1,19 @@
+'''numeros = (5, 'zero', 4, 6, 'alo', 7)
+
+for numero in numeros:
+	print (numero)
+
+print(numeros)
+print(numeros[2])
+print(numeros[-1])
+
+numero[1] = 0'''
+
+'''numeros = (1, 2, 3, 4, 5, 5, 5, 5, 7, 8)
+print(numeros.count(5))'''
+
+a = (1, 2, 3)
+b = (4, 5, 6)
+
+c = a + b
+print(c)
