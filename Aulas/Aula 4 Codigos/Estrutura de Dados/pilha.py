5839e68382b5171210df56cc68842defa43c999d
mateusmdc <38740703+mateusmdc@users.noreply.github.com>
2018-12-10 10:45:18 -0300

    Create pilha.py

---
+++ Aulas/Aula 4 Codigos/Estrutura de Dados/pilha.py
@@ -0,0 +1,19 @@
+class PIlha(object):
+	def __init__(self):
+		self.dados = []
+
+	def empilha(self.elemento):
+		self.dados.append(elemento)
+
+	def desempilha(self):
+		return self.dados.pop()
+
+def Fila(object):
+	def __init__(self)
+		self.dados = []
+
+	def queue(self.elemento):
+		self.dados.append(elemento)
+
+	def dequeue(self):
+		return self.dados.pop(0)
