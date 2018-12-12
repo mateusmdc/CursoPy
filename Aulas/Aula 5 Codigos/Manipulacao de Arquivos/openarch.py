da222cdc2fc76ec6461beba791fa8f83eea505ad
mateusmdc <38740703+mateusmdc@users.noreply.github.com>
2018-12-12 10:43:58 -0300

    Create openarch.py

---
+++ Aulas/Aula 5 Codigos/Manipulacao de Arquivos/openarch.py
@@ -0,0 +1,86 @@
+#arquivo = open('teste.txt', 'w')
+
+#arquivo.write('Oi, eu sou o goku')
+
+#texto = """#conhece?
+#1
+#2
+#3
+#4
+#5
+#7
+#8"""
+
+
+#arquivo.write(texto)
+
+#arquivo.close()
+
+
+#arquivo.read()   
+
+
+'''arquivo = open('teste.txt', 'r')
+
+string = arquivo.read()
+
+print(string)
+
+arquivo.close()'''
+
+#arquivo = open('teste.txt', 'r')
+
+'''linha1 = arquivo.readline()
+linha2 = arquivo.readline()
+linha3 = arquivo.readline()
+linha4 = arquivo.readline()
+print(linha1)
+print(linha2)
+print(linha3)
+print(linha4)'''
+
+#arquivo = open('teste.txt', 'r')
+
+'''lista = arquivo.readlines()
+print(lista)
+
+print(lista[3])
+
+arquivo.close'''
+ 
+
+
+#arquivo = open('teste.txt', 'r')
+
+'''lista = arquivo.readlines()
+
+print(lista.pop(2))
+
+lista.remove("#7\n")
+
+lista.pop(3)
+
+print(lista)
+
+arquivo.close
+
+arquivo = open('teste.txt', 'w')
+arquivo.writelines(lista)
+arquivo.close()'''
+
+
+#arquivo = open('teste.txt', 'r')
+'''lista = arquivo.readlines()
+
+lista.append("\n#9\n")
+
+print(lista)
+
+arquivo.close()
+
+arquivo = open('teste.txt', 'w')
+
+lista = arquivo.writelines(lista)
+
+
+arquivo.close'''
